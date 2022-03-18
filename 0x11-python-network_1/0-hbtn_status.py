308066cb942b0a17a90620ba8f6810fcd9cdef90
Mikiyas <65415165+Monange1@users.noreply.github.com>
2022-03-18 22:38:10 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,8 @@
-#!/usr/bin/python2
-"""
-Fetches https://intranet.hbtn.io/status
-use the package urllib
- body of the response must be displayed in tabulation before -
-"""
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+if __name__ == "__main__":
+    import urllib.request
 
-print('Body response:\n\t- type: {}'.format(type(html)))
-print('\t- content: {}'.format(html))
-print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+            as response:
+        html = response.read()
+    print("""Body response:\n    - type: {}\n    - content: {}
+    - utf8 content: {}""".format(type(html), html, html.decode('utf-8')))
